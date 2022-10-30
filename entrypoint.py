eacb8dfceca1edb534d4bfa6a8838bcd22c465e3
Dubhar <faebhir@gmail.com>
2022-10-30 22:24:47 +0100

    default url

--- entrypoint.py
+++ entrypoint.py
@@ -5,7 +5,10 @@ import exampleProto.test_pb2_grpc as service
 
 from google.protobuf import empty_pb2 as empty
 
-channel = grpc.insecure_channel(os.environ['URL'])
+url = "host.docker.internal:6565"
+if 'URL' in os.environ:
+  url = os.environ['URL']
+channel = grpc.insecure_channel(url)
 message = empty.Empty()
 stub = service.SaySomethingStub(channel)
 answer = stub.Hello(message)
